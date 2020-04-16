f3e957b5ae6329742e8b446091370a8cb9ca6126
Dante Astudillo <daac99@gmail.com>
2020-04-16 02:23:26 -0500

    enable serverconfig added the config for git in yaml file

--- spring-cloud-config-server/src/main/java/com/mx/daac/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/mx/daac/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.mx.daac.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
